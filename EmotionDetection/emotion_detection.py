6ffd176802aa8ef24aaddd2afef4f1fde0753581
Sjutzu <daniel.zentlik6@gmail.com>
2026-05-07 15:13:10 -0400

    Handling blank inputs sent by users

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -20,7 +20,27 @@ def emotion_detector(text_to_analyze):
         }
     }
     response = requests.post(url, headers=header, json=my_obj, timeout=30)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    elif response.status_code == 400:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    else:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     return emotions
+    
--- server.py
+++ server.py
@@ -11,6 +11,8 @@ def text_analyzer():
     """ Analyzing text given at the website """
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     return (
         f"For the given statement, the system response is 'anger': {result['anger']},\n"
         f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and\n"
